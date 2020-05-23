c9a543d10acc48b0ec983368bb3b4091e5a65966
truong081294 <truonggiagianguyen@gmail.com>
2020-05-23 18:08:18 +0700

    first commit

---
+++ resources/js/components/ListUser.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="list-user">
+        <table class="tbl-list-user">
+            <thead>
+            <th>Name</th>
+            <th>Location</th>
+            <th>DOB</th>
+            </thead>
+            <tbody>
+            <tr v-for="user in userData" @click="selectUser(user)">
+                <td><a>{{ user.name.title }} - {{ user.name.last }} {{ user.name.first }}</a></td>
+                <td>{{ user.location.city }}</td>
+                <td>{{ user.dob.date }}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    import data from './data.json'
+    export default {
+        data() {
+            return {
+                userData: data
+            }
+        },
+        methods: {
+            selectUser(user) {
+                this.$emit('userSelected', user)
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .tbl-list-user {
+        width: 100%;
+    }
+</style>
---
+++ resources/js/components/UserDashboard.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="user-dashboard">
+        <div class="list-user-comp">
+            <h4>List User</h4>
+            <ListUser @userSelected="childrenSelectUser"></ListUser>
+        </div>
+        <hr>
+        <div class="user-detail-comp">
+            <h4>User Detail</h4>
+            <UserDetail :userDetailInfo="userSelectedFromChild"></UserDetail>
+        </div>
+    </div>
+</template>
+
+<script>
+    import ListUser from './ListUser.vue'
+    import UserDetail from './UserDetail.vue'
+    export default {
+        components: {
+            ListUser,
+            UserDetail
+        },
+        data() {
+            return {
+                userSelectedFromChild: {}
+            }
+        },
+        methods: {
+            // Process event emitted from child
+            childrenSelectUser(user) {
+                console.log(user)
+                this.userSelectedFromChild = user
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .tbl-list-user {
+        width: 100%;
+    }
+</style>
